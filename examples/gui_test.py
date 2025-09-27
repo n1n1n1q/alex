4bb64cf338ed5344b4895132ceedd73e7e3c428d
annastasyshyn <anna.stasyshynn@gmail.com>
2025-09-27 14:10:11 +0300

    add gui example for testing

---
+++ examples/gui_test.py
@@ -0,0 +1,18 @@
+from minestudio.simulator import MinecraftSim
+from minestudio.simulator.callbacks import PlayCallback
+
+sim = MinecraftSim(
+    action_type="env",
+    callbacks=[
+        PlayCallback()
+    ]
+)
+
+obs, info = sim.reset()
+terminated = False
+
+while not terminated:
+    action = None
+    obs, reward, terminated, info = sim.step(action)
+
+sim.close()

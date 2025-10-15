0e5f3031bdb88e987ed0ca825ed6c8b780cce4e8
annastasyshyn <anna.stasyshynn@gmail.com>
2025-10-15 17:30:45 +0300

    better policy reward

--- examples/training/train_utils.py
+++ examples/training/train_utils.py
@@ -6,7 +6,7 @@ def policy_generator():
     # weights_path = '/alex/models/foundation-2x/model.safetensors'
     # policy = load_vpt_policy(model_path, None)
     # return policy
-    return VPTPolicy.from_pretrained("CraftJarvis/MineStudio_VPT.foundation_model_2x")
+    return VPTPolicy.from_pretrained("CraftJarvis/MineStudio_VPT.foundation_model_3x")
 
 
 def env_generator():
@@ -26,27 +26,36 @@ def env_generator():
         action_type = "agent",
         timestep_limit=1000,
         callbacks=[
-            SummonMobsCallback([{'name': 'cow', 'number': 10, 'range_x': [-15, 15], 'range_z': [-15, 15]}]),
+            SummonMobsCallback([{'name': 'cow', 'number': 20, 'range_x': [-10, 10], 'range_z': [-10, 10]}]),
             MaskActionsCallback(inventory=0), 
-            RewardsCallback([{
+            RewardsCallback([
+            {
                 'event': 'kill_entity', 
                 'objects': ['cow'], 
-                'reward': 1.0, 
+                'reward': 5.0,
                 'identity': 'kill_cow', 
-                'max_reward_times': 30, 
-            }]),
+                'max_reward_times': 50, 
+            },
+            {
+                'event': 'damage_dealt',
+                'objects': ['cow'],
+                'reward': 0.1,
+                'identity': 'damage_cow',
+                'max_reward_times': 500,
+            }
+        ])
             CommandsCallback(commands=[
-                # '/give @p minecraft:iron_sword 1',
+                '/give @p minecraft:netherite_sword 1',
                 # '/give @p minecraft:diamond 64',
-                # '/effect @p 5 9999 255 true',
-                '/effect clear @p',
+                '/effect @p 5 9999 255 true',
+                # '/effect clear @p',
             ]),
             FastResetCallback(
                 biomes=['plains'],
                 random_tp_range=1000,
             ),
             JudgeResetCallback(600),
-            RecordCallback('./records/train_cow'),
+            RecordCallback('./records/kill_cow'),
         ]
     )
     return sim
